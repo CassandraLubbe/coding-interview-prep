001a53aa19c7beb61a47fd271eeb35ca1f4c5b37
Cassandra Lubbe <147734119+CassandraLubbe@users.noreply.github.com>
2025-04-09 12:37:35 +0200

    Create merge_sorted_array.py

---
+++ Top 150 Interview Questions/merge_sorted_array.py
@@ -0,0 +1,16 @@
+## Merge Sorted Array LeetCode
+## 88 [Merge Sorted Array](https://leetcode.com/problems/merge-sorted-array/description/) 
+
+def merge(self, nums1, m, nums2, n):
+        i = m - 1
+        j = n - 1
+        k = m + n - 1 
+
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
